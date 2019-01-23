1d57606fc37da13e5b018eb5a04a16dae8ef5326
Ashley Levi <Ashplevi@gmail.com>
2019-01-23 09:49:38 -0700

    Make dom show random colors on generate click

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -2,6 +2,19 @@ const generateBtn = document.querySelector('.generate-btn');
 const lockBtn = document.querySelector('.fa-unlock-alt');
 const saveBtn = document.querySelector('.save-btn');
 const deleteBtn = document.querySelector('.delete-btn');
+const colorOne = document.querySelector('.color1');
+const colorTwo = document.querySelector('.color2');
+const colorThree = document.querySelector('.color3');
+const colorFour = document.querySelector('.color4');
+const colorFive = document.querySelector('.color5');
+const hexOne = document.querySelector('.hex1');
+const hexTwo = document.querySelector('.hex2');
+const hexThree = document.querySelector('.hex3');
+const hexFour = document.querySelector('.hex4');
+const hexFive = document.querySelector('.hex5');
+
+
+
 
 generateHex = () => {
   let color = '#';
@@ -21,3 +34,19 @@ generateColors = () => {
   }
   return colors
 }
+
+displayColors = () => {
+  let colors = generateColors()
+  colorOne.setAttribute('style', `background-color: ${colors[0]}`)
+  hexOne.innerText = colors[0]
+  colorTwo.setAttribute('style', `background-color: ${colors[1]}`)
+  hexTwo.innerText = colors[1]
+  colorThree.setAttribute('style', `background-color: ${colors[2]}`)
+  hexThree.innerText = colors[2]
+  colorFour.setAttribute('style', `background-color: ${colors[3]}`)
+  hexFour.innerText = colors[3]
+  colorFive.setAttribute('style', `background-color: ${colors[4]}`) 
+  hexFive.innerText = colors[4]
+}
+
+generateBtn.addEventListener('click', displayColors)
