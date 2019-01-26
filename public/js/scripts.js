1448b443c8c7074d3dc7a88ea31cf1fd10811f96
Ashley Levi <Ashplevi@gmail.com>
2019-01-26 13:45:37 -0700

    Comment out

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -24,7 +24,6 @@ let savedColorsDisplay = document.querySelector('.saved-colors-display');
 
 displayColors()
 fetchAllProjects()
-fetchAllPalettes()
 
 
 generateBtn.addEventListener('click', displayColors)
@@ -108,12 +107,12 @@ function fetchPalettes(projects) {
 }
 
 
-function fetchAllPalettes() {
-  fetch('/api/v1/projects')
-  .then(response => response.json())
-  .then(palettes => console.log(palettes))
-  .catch(error => console.log(error))
-} 
+// function fetchAllPalettes() {
+//   fetch('/api/v1/projects')
+//   .then(response => response.json())
+//   .then(palettes => console.log(palettes))
+//   .catch(error => console.log(error))
+// } 
 
 
 function saveProject() {
